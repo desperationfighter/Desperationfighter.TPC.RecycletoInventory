eb6c1fbfef892a3dbe065ecaefbce660376001e4
Desperationfighter <patrick_as_desp@yahoo.de>
2023-11-09 22:49:28 +0100

    Add project files.

---
+++ RecycletoInventory/Patches/ActionRecycle_Patches.cs
@@ -0,0 +1,107 @@
+﻿using HarmonyLib;
+using SpaceCraft;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RecycletoInventory.Patches
+{
+    [HarmonyPatch(typeof(ActionRecycle))]
+    [HarmonyPatch(nameof(ActionRecycle.OnAction))]
+    public static class ActionRecycle_Patches
+    {
+        [HarmonyPrefix]
+        static bool Prefix(ActionRecycle __instance)
+        {
+            if (!Plugin.ModisActive.Value) return true;
+
+            OnAction_Patch(__instance);
+
+            return false;
+        }
+
+        static void OnAction_Patch(ActionRecycle __instance)
+        {
+            __instance.tempCage.SetActive(true);
+            Inventory inventory = __instance.GetComponentInParent<InventoryAssociated>().GetInventory();
+            if (inventory.GetInsideWorldObjects().Count == 0)
+            {
+                return;
+            }
+            WorldObject worldObject = inventory.GetInsideWorldObjects()[0];
+            List<Group> ingredientsGroupInRecipe = worldObject.GetGroup().GetRecipe().GetIngredientsGroupInRecipe();
+
+            //Get Play Inventory for Later Checks
+            Inventory inventory_Player = Managers.GetManager<PlayersManager>().GetActivePlayerController().GetPlayerBackpack().GetInventory();
+            //
+            GlobalAudioHandler globalAudioHandler = Managers.GetManager<GlobalAudioHandler>();
+            //Set Warning Flag to intial State
+            bool alreadywarned = false;
+
+            if (ingredientsGroupInRecipe.Count == 0 || ((GroupItem)worldObject.GetGroup()).GetCantBeRecycled())
+            {
+                WorldObject worldObject_tmp = WorldObjectsHandler.CreateAndDropOnFloor(worldObject.GetGroup(), __instance.craftSpawn.transform.position, 0f);
+                if (inventory_Player.IsFull())
+                {
+                    if (!alreadywarned)
+                    {
+                        alreadywarned = true;
+                    }
+                }
+                else
+                {
+                    if (inventory_Player.AddItem(worldObject_tmp))
+                    {
+                        Object.Destroy(worldObject_tmp.GetGameObject());
+                        worldObject_tmp.SetDontSaveMe(false);
+                    }
+                }
+            }
+            else
+            {
+
+                foreach (Group group in ingredientsGroupInRecipe)
+                {
+                    Vector3 vector = new Vector3(Random.Range(__instance.craftSpawn.bounds.min.x, __instance.craftSpawn.bounds.max.x), Random.Range(__instance.craftSpawn.bounds.min.y, __instance.craftSpawn.bounds.max.y), Random.Range(__instance.craftSpawn.bounds.min.z, __instance.craftSpawn.bounds.max.z));
+                    WorldObject worldObject_tmp = WorldObjectsHandler.CreateAndDropOnFloor(group, vector, 0.6f);
+
+                    if (inventory_Player.IsFull())
+                    {
+                        if (!alreadywarned)
+                        {
+                            alreadywarned = true;
+                        }
+                    }
+                    else
+                    {
+                        if (inventory_Player.AddItem(worldObject_tmp))
+                        {
+                            Object.Destroy(worldObject_tmp.GetGameObject());
+                            worldObject_tmp.SetDontSaveMe(false);
+                        }
+                    }
+                }
+            }
+
+            if (alreadywarned)
+            {
+                Managers.GetManager<BaseHudHandler>().DisplayCursorText("UI_WARN_RECYLCE_INV_FULL", 2f, "Inventory Full");
+                if (Plugin.Sound.Value)
+                {
+                    globalAudioHandler.PlayDropObject();
+                }
+            }
+            else
+            {
+                Managers.GetManager<BaseHudHandler>().DisplayCursorText("UI_INFO_RECYLCE_TO_INV", 2f, "Transfered to Inventory");
+                if (Plugin.Sound.Value)
+                {
+                    globalAudioHandler.PlayUiSelectElement();
+                }
+            }
+
+            inventory.RemoveItem(worldObject, true);
+            __instance.Invoke("DisableCage", 1.5f);
+            __instance.OnAction();
+        }
+    }
+}
---
+++ RecycletoInventory/Plugin.cs
@@ -0,0 +1,43 @@
+﻿using BepInEx.Configuration;
+using BepInEx.Logging;
+using BepInEx;
+using HarmonyLib;
+using System.Reflection;
+
+namespace RecycletoInventory
+{
+    [BepInPlugin(GUID, Name, Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public new static ManualLogSource Logger { get; private set; }
+        public const string GUID = "Desperationfighter.TPC.RecycletoInventory";
+        public const string Name = "Recycle to Inventory";
+        public const string Version = "1.0.0.0"; //Remmber to Update Assembly Version too !
+
+        public static ConfigEntry<bool> ModisActive;
+        public static ConfigEntry<bool> Debuglogging;
+        public static ConfigEntry<bool> Sound;
+
+        private void Awake()
+        {
+            ModisActive = Config.Bind("1_General", "ModisActive", true, "Set if the Mod should running or not. If you don't want to remove Files or for Later Ingame Menu. Please reload your Savegame after Change as there are Setting that only apply once when World is loaded up.");
+            Debuglogging = Config.Bind("9_Advanced", "Debuglogging", false, "Enables Debug Logging. Should be only activated when you know what you do.");
+            Sound = Config.Bind("2_Config", "Sound", true, "Gives Audio Feedback or not.");
+
+            // set project-scoped logger instance
+            Logger = base.Logger;
+
+            // register harmony patches, if there are any
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), GUID);
+            Logger.LogInfo($"Plugin {GUID} is loaded!");
+        }
+
+        public static void MyDebugLogger(string message)
+        {
+            if (Debuglogging.Value)
+            {
+                Logger.LogDebug($"[{Name}][Debug] : {message} [/Debug]");
+            }
+        }
+    }
+}
